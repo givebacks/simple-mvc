403f0cbb889cda6bb08f2409e5fd3373b2b91377
Victor <106319089+givebacks@users.noreply.github.com>
2022-05-27 20:30:13 -0300

    Add files via upload

--- simple-mvc/app/Model/SQL.php
+++ simple-mvc/app/Model/SQL.php
@@ -9,6 +9,7 @@ class SQL
   	private $stmt;
   	public  $error = [];
   
+
 	/* Create Dbh instance */
 
 	public function __construct($obj = NULL)
@@ -21,20 +22,53 @@ class SQL
 		return $this->error[0];
 	}
 
+	/* 
+	
+	public function fetch() returns the result
+
+	Ex: $this->query($stmt,$data) ? $this->fetch() : $this->error();
+	
+	*/
+
 	public function fetch($callback = NULL)
 	{
 		return $this->stmt->fetch($callback);
 	}
+
+	/* 
+	
+	public function fetchAll() returns all result as an array
+
+	Ex: $this->query($stmt,$data) ? $this->fetchAll() : $this->error();
+	
+	*/
 
 	public function fetchAll($callback = NULL)
 	{
 		return $this->stmt->fetchAll($callback);
 	}
 
+	/* 
+	
+	public function lastInsertedId() returns last inserted data ID 
+
+	Ex: $this->query($stmt,$data) ? $this->lastInsertedId() : $this->error();
+	
+	*/
+
 	public function lastInsertedId()
 	{
 		return $this->stmt->lastInsertId();
 	}
+
+	
+	/* 
+	
+	public function rows() returns n of affected rows on update for example
+
+	Ex: $this->query($stmt,$data) ? $this->rows() : $this->error();
+	
+	*/
 
 	public function rows()
 	{
