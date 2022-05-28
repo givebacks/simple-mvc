f27317b5c19fdcb40dd63adb784b2f1e89d12b3f
Victor <106319089+givebacks@users.noreply.github.com>
2022-05-27 22:22:24 -0300

    Add files via upload

--- simple-mvc/public/index.php
+++ simple-mvc/public/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
 require_once "../config/config.php";
-require_once "../src/vendor/autoload.php";
+require_once "../vendor/autoload.php";
 
 
 
